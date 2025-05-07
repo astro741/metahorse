0753e0d60795d30a8cf41dc22d8e612809dfd90f
astro741 <williammorphy37@gmail.com>
2025-05-07 18:58:57 +0300

    Init commit

---
+++ src/landingcomponents/AnimatedSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import { useIsInViewport } from "./useIsInViewport";
+export const AnimatedSection = ({ getStyles, children }) => {
+  const elementRef = React.useRef();
+  const isInViewport = useIsInViewport(elementRef);
+
+  return (
+    <section style={getStyles(isInViewport)}>
+      <div className="rectangle" ref={elementRef}>
+        {children}
+      </div>
+    </section>
+  );
+};
